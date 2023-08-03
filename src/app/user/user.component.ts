ff4085afae9693339cfce25365695d5e4448022b
Lothar Zok <111051858+LotharZok@users.noreply.github.com>
2023-08-03 09:04:23 +0200

    add user dialog

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
+import { MatDialogModule, MatDialog } from '@angular/material/dialog';
+import { DialogAddUserComponent } from '../dialog-add-user/dialog-add-user.component';
 
 @Component({
-  selector: 'app-user',
-  templateUrl: './user.component.html',
-  styleUrls: ['./user.component.scss']
+    selector: 'app-user',
+    templateUrl: './user.component.html',
+    styleUrls: ['./user.component.scss']
 })
 export class UserComponent {
 
+    constructor(public dialog: MatDialog) {
+
+    }
+
+    openDialog(): void {
+        this.dialog.open(DialogAddUserComponent);
+    }
 }
