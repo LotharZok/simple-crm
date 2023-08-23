d8ee96890858c03e7972bd5f34cee2042e00aa26
Lothar Zok <111051858+LotharZok@users.noreply.github.com>
2023-08-23 09:10:28 +0200

    styling of company view

--- src/app/companies/companies.component.ts
+++ src/app/companies/companies.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { Firestore, collection, collectionData } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-companies',
@@ -7,4 +9,109 @@ import { Component } from '@angular/core';
 })
 export class CompaniesComponent {
 
+    firestore: Firestore = inject(Firestore);              // Zugriff auf Firestore
+    compCollection = collection(this.firestore, 'users');  // Holen der Sammlung 'users'
+    companies$: Observable<any> = collectionData
+    (this.compCollection, { idField: 'id' }); // Zugriff auf die Daten der Sammlung, d.h. die einzelnen Einträge. Inkl. die ID eines Eintrags ins neue Feld id.
+    allCompanies: string[] = [];
+
+
+    openDialog() {
+        console.log('openDialog wurde gestartet');
+
+    }
+
+    deleteCompany(id: string) {
+        console.log('deleteCompany wurde gestartet');
+        // OpenDialog stoppen
+        event?.stopPropagation();
+    }
+
+    // ---------- TEST ---------- //
+    expandContent = true;
+    expandDept = true;
+
+    testCompanies = [
+        {
+            name: 'A A & Co.',
+            city: 'Augsburg',
+            web: 'www.a-a.com',
+            expanded: false,
+            employees: [
+                {
+                    lastname: 'Aschenbrenner',
+                    firstname: 'Adam',
+                    mail: 'aasb@a-a.com'
+                },
+                {
+                    lastname: 'Bodewig',
+                    firstname: 'Barbara',
+                    mail: 'bbod@a-a.com'
+                },
+                {
+                    lastname: 'Cramer',
+                    firstname: 'Chris',
+                    mail: 'ccrm@a-a.com'
+                }
+            ]
+        },
+        {
+            name: 'Bee GmbH',
+            city: 'Biberach',
+            web: 'www.bee.de',
+            expanded: false,
+            employees: [
+                {
+                    lastname: 'Diederichs',
+                    firstname: 'Doro',
+                    mail: 'doro-diederichs@bee.de'
+                },
+                {
+                    lastname: 'Eberhardt',
+                    firstname: 'Emil',
+                    mail: 'emil-eberhardt@bee.de'
+                },
+                {
+                    lastname: 'Finkel',
+                    firstname: 'Fee',
+                    mail: 'fee-finkel@bee.de'
+                }
+            ]
+        },
+        {
+            name: 'Ceh & Taumen',
+            city: 'Chemnitz',
+            web: 'www.c-und-t.eur',
+            expanded: false,
+            employees: []
+        },
+        {
+            name: 'D-E-F AG',
+            city: 'Dormagen',
+            web: 'www.d-e-f.de',
+            expanded: false,
+            employees: [
+                {
+                    lastname: 'Greiner',
+                    firstname: 'Georg',
+                    mail: 'greiner@d-e-f.de'
+                }
+            ],
+            departments: [
+                {
+                    name: 'Einkauf',
+                    city: 'Korschenbroich',
+                    mail: 'einkauf@d-e-f.de',
+                    expanded: false,
+                    employees: [
+                        {
+                            lastname: 'Holzweiler',
+                            firstname: 'Hannah',
+                            mail: 'holzweiler@d-e-f.de'
+                        }
+                    ]
+                }
+            ]
+        }
+    ];
 }
